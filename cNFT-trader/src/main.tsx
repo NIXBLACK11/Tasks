64e686659aad383af748cc833138f29518ffc375
NIXBLACK11 <siddharthsinghrana11@gmail.com>
2025-01-10 15:05:40 +0530

    correct global

--- cNFT-trader/src/main.tsx
+++ cNFT-trader/src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx';
 
 import { Buffer } from 'buffer';
 window.Buffer = Buffer;
+window.global ||= window;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
